a4289ad406cd54c88cbdc3afd463c744faff6e9b
JFernandez180717 <cta.cv2014@gmail.com>
2025-10-24 20:14:46 -0500

    add backend with nest js with prisma

---
+++ back/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Disponible en todos los módulos
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+})
+export class AppModule {}
